88b35272431c14999b3e0ccda2b4db4cbfd143d5
Drakodav <vmedves@gmail.com>
2024-03-19 23:12:49 +0000

    update asset prefix

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,6 +1,10 @@
 import * as path from "path";
 import { defineConfig } from "rspress/config";
 
+const isProd = process.env.NODE_ENV === "prod";
+
+const base = isProd ? "foh-productions/" : "/";
+
 export default defineConfig({
   root: path.join(__dirname, "docs"),
   title: "foh-productions",
@@ -9,6 +13,12 @@ export default defineConfig({
   logo: {
     light: "/rspress-light-logo.png",
     dark: "/rspress-dark-logo.png",
+  },
+  base: base,
+  builderConfig: {
+    output: {
+      assetPrefix: base,
+    },
   },
   themeConfig: {
     lastUpdated: true,
